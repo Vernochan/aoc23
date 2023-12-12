3aa7037bea12a8d504b45abe8b774ccb7ad4a1b6
Patrick <patrick.wegner@rewe-group.com>
2023-12-12 23:55:56 +0100

    finished aoc12

--- aoc12/aoc12.go
+++ aoc12/aoc12.go
@@ -1,29 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-// type Point struct {
-// 	x int
-// 	y int
-// }
+var cache = make(map[string]int)
+var cacheHits = 0
 
 func main() {
 	lines := ReadFile("test.txt")
 
 	sum := 0
+	sum2 := 0
 	for _, x := range lines {
 		sum += getTotalConfigurations(x)
+		sum2 += getTotalConfigurationsExpanded(x)
 	}
+
+	fmt.Println("Puzzle 1: ", sum)
+	fmt.Println("Puzzle 1: ", sum2, "Cache hits: ", cacheHits)
 }
 
+func getTotalConfigurationsExpanded(line string) int {
+	springs := strings.Fields(line)[0]
+
+	numbersStrings := strings.Split(strings.Fields(line)[1], ",")
+	numbers := make([]int, 0)
+
+	for _, x := range numbersStrings {
+		i, _ := strconv.Atoi(x)
+		numbers = append(numbers, i)
+	}
+
+	expandedNumbers := make([]int, 0)
+	for i := 0; i < 5; i++ {
+		expandedNumbers = append(expandedNumbers, numbers...)
+
+	}
+
+	expandedStrings := fmt.Sprintf("%[1]s?%[1]s?%[1]s?%[1]s?%[1]s", springs)
+
+	count := getConfigurations(expandedStrings, expandedNumbers)
+
+	return count
+}
 func getTotalConfigurations(line string) int {
 	springs := strings.Fields(line)[0]
-	numbersString := strings.Fields(line)[1]
-	numbersStrings := strings.Split(numbersString, ",")
+	numbersStrings := strings.Split(strings.Fields(line)[1], ",")
 	numbers := make([]int, 0)
 
 	for _, x := range numbersStrings {
@@ -36,35 +62,65 @@ func getTotalConfigurations(line string) int {
 	return count
 }
 
-func getConfigurations(line string, nums []int) int {
-	if len(line) == 0 || len(nums) == 0 {
+func getConfigurations(springLine string, springLengths []int) int {
+	cacheKey := springLine
+	for _, num := range springLengths {
+		cacheKey += strconv.Itoa(num) + ","
+	}
+
+	if cacheValue, ok := cache[cacheKey]; ok {
+		cacheHits++
+		return cacheValue
+	}
+	if len(springLine) == 0 {
+		if len(springLengths) == 0 {
+			return 1
+		}
 		return 0
 	}
-	retVal := 0
-	potentialIdx := strings.Index(line, "?")
-	springLen := 0
-	numIndex := 0
-	for i := 0; i < potentialIdx; i++ {
 
-		// Fallunterscheidung:
-		// 1 => zusammenhängende Spring (? oder #)     => retVal += 1 + getconfugurations(string[nums[0]:], nums[1:])
-		// 2 => hängt nicht zusammen/gehört nicht dazu => retVal += getconfigurations(string[1:], nums)
+	// Fallunterscheidung:
+	// 1 => zusammenhängende Spring (? oder #)     => retVal += 1 + getconfugurations(string[nums[0]:], nums[1:])
+	// 2 => hängt nicht zusammen/gehört nicht dazu => retVal += getconfigurations(string[1:], nums)
 
-		if line[i] == '#' {
-			springLen++
-			if springLen == nums[numIndex] {
-				numIndex++
-			}
-		}
-
-		if line[i] == '?' {
-			//lookahead and count #
-			retVal = 1 // decoy so path is not empty
-		}
-
+	if strings.HasPrefix(springLine, ".") {
+		val := getConfigurations(strings.TrimPrefix(springLine, "."), springLengths)
+		cache[cacheKey] = val
+		return val
 	}
 
-	return retVal
+	if strings.HasPrefix(springLine, "?") {
+		return getConfigurations(fmt.Sprintf(".%s", springLine[1:]), springLengths) +
+			getConfigurations(fmt.Sprintf("#%s", springLine[1:]), springLengths)
+	}
+
+	// line must start with # (no other option left)
+	if len(springLengths) == 0 {
+		cache[cacheKey] = 0
+		return 0
+	}
+	if len(springLine) < springLengths[0] {
+		cache[cacheKey] = 0
+		return 0
+	}
+	if strings.Contains(springLine[0:springLengths[0]], ".") {
+		cache[cacheKey] = 0
+		return 0
+	}
+	if len(springLengths) == 1 {
+		val := getConfigurations(springLine[springLengths[0]:], springLengths[1:])
+		cache[cacheKey] = val
+		return val
+	}
+
+	if len(springLine) < springLengths[0]+1 || string(springLine[springLengths[0]]) == "#" {
+		cache[cacheKey] = 0
+		return 0
+	}
+	val := getConfigurations(springLine[springLengths[0]+1:], springLengths[1:])
+	cache[cacheKey] = val
+	return val
+
 }
 
 func ReadFile(fName string) []string {
--- aoc12/aoc12_test.go
+++ aoc12/aoc12_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_getConfigurations(t *testing.T) {
 	type args struct {
@@ -29,6 +31,31 @@ func Test_getConfigurations(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := getTotalConfigurations(tt.args.line); got != tt.want {
 				t.Errorf("getConfigurations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getTotalConfigurationsExpanded(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"???.### 1,1,3"}, 1},
+		{"2", args{".??..??...?##. 1,1,3"}, 16384},
+		{"3", args{"?#?#?#?#?#?#?#? 1,3,1,6"}, 1},
+		{"4", args{"????.#...#... 4,1,1"}, 16},
+		{"5", args{"????.######..#####. 1,6,5"}, 2500},
+		{"6", args{"?###???????? 3,2,1"}, 506250},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getTotalConfigurationsExpanded(tt.args.line); got != tt.want {
+				t.Errorf("getTotalConfigurationsExpanded() = %v, want %v", got, tt.want)
 			}
 		})
 	}
